c63d390e8a5350252226aa434c4362cce8bd2877
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-01-04 15:34:43 +0000

    IDVA6-369 The landing page path changed and navbar and test updated

--- test/src/routers/yourCompaniesRouter.test.ts
+++ test/src/routers/yourCompaniesRouter.test.ts
@@ -2,8 +2,8 @@ import app from "../../../src/app";
 import supertest from "supertest";
 const router = supertest(app);
 
-describe("GET /", () => {
+describe("GET /your-companies", () => {
     it("should return status 200", async () => {
-        await router.get("/").expect(200);
+        await router.get("/your-companies").expect(200);
     });
 });
