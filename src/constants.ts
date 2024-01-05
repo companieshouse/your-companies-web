7840d619d39a56fba4919684b13d8ba8a2bb9957
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-01-05 13:45:08 +0000

    IDVA6-339 Hard coded paths and file names replaced with constants

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+// Paths to Nunjucks template files
+export const ERROR_400_TEMPLATE = "partials/error_400";
+export const YOUR_COMPANIES_TEMPLATE_FOLDER = "router_views/your_companies";
+export const YOUR_COMPANIES_PAGE_TEMPLATE = `${YOUR_COMPANIES_TEMPLATE_FOLDER}/your_companies`;
+
+// English and Welsh translation files
+export const YOUR_COMPANIES_LANG = "your-companies";
+
+// Routing paths
+export const LANDING_URL = "/your-companies";
+export const ADD_COMPANY_URL = "/add-company";
+export const YOUR_COMPANIES_URL = "/";
+export const YOUR_COMPANIES_ADD_COMPANY_URL = `${LANDING_URL}${ADD_COMPANY_URL}`;
