bee2373a29f29447e94dcb48e1c7e30a27f80c90
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2024-01-09 11:15:26 +0000

    Changing terraform folder structure

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -1,28 +1,45 @@
 # Define all hardcoded local variable and local variables looked up from data resources
 locals {
-  stack_name                = "identity" # this must match the stack name the service deploys into
-  name_prefix               = "${local.stack_name}-${var.environment}"
-  global_prefix             = "global-${var.environment}"
-  service_name              = "your-companies-web"
-  container_port            = "3000" # default Node port to match start script
-  docker_repo               = "your-companies-web"
-  lb_listener_rule_priority = 14
-  lb_listener_paths         = [""]
-  healthcheck_path          = "/healthcheck" #healthcheck path for accounts association service
-  healthcheck_matcher       = "200"
+  stack_name                 = "identity" # this must match the stack name the service deploys into
+  name_prefix                = "${local.stack_name}-${var.environment}"
+  global_prefix              = "global-${var.environment}"
+  service_name               = "your-companies-web"
+  container_port             = "3000" # default Node port to match start script
+  docker_repo                = "your-companies-web"
+  lb_listener_rule_priority  = 18
+  lb_listener_paths          = ["/your-companies/*"]
+  healthcheck_path           = "/your-companies" #healthcheck path for accounts association service
+  healthcheck_matcher        = "200"
   application_subnet_ids     = data.aws_subnets.application.ids
-  kms_alias                 = "alias/${var.aws_profile}/environment-services-kms"
-  service_secrets           = jsondecode(data.vault_generic_secret.service_secrets.data_json)
+  kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
+  service_secrets            = jsondecode(data.vault_generic_secret.service_secrets.data_json)
   stack_secrets              = jsondecode(data.vault_generic_secret.stack_secrets.data_json)
   application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
   use_set_environment_files  = var.use_set_environment_files
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
   app_environment_filename   = "your-companies-web.env"
   parameter_store_secrets    = {
-    "vpc_name"                        = local.vpc_name
+    "vpc_name"                  = local.vpc_name
+    "chs_api_key"               = local.service_secrets["chs_api_key"]
+    "chs_internal_api_key"      = local.service_secrets["chs_internal_api_key"]
+    "internal_api_url"          = local.service_secrets["internal_api_url"]
+    "cdn_host"                  = local.service_secrets["cdn_host"]
+    "oauth2_auth_uri"           = local.service_secrets["oauth2_auth_uri"]
+    "oauth2_redirect_uri"       = local.service_secrets["oauth2_redirect_uri"]
+    "account_url"               = local.service_secrets["account_url"]
+    "cache_server"              = local.service_secrets["cache_server"]
   }
 
-  vpc_name                         = local.service_secrets["vpc_name"]
+  vpc_name                  = local.service_secrets["vpc_name"]
+  chs_api_key               = local.service_secrets["chs_api_key"]
+  chs_internal_api_key      = local.service_secrets["chs_internal_api_key"]
+  internal_api_url          = local.service_secrets["internal_api_url"]
+  cdn_host                  = local.service_secrets["cdn_host"]
+  oauth2_auth_uri           = local.service_secrets["oauth2_auth_uri"]
+  oauth2_redirect_uri       = local.service_secrets["oauth2_redirect_uri"]
+  account_url               = local.service_secrets["account_url"]
+  cache_server              = local.service_secrets["cache_server"]
+
 
   # create a map of secret name => secret arn to pass into ecs service module
   # using the trimprefix function to remove the prefixed path from the secret name
