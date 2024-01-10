ee4f671db863426a434b441f31b34bf25f2c13e8
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-01-10 10:19:53 +0000

    IDVA6-339 Method signature updated and error logging added

--- src/services/accountUserCompanyServiceClient.ts
+++ src/services/accountUserCompanyServiceClient.ts
@@ -1,7 +1,8 @@
 import Axios, { AxiosInstance } from "axios";
 import { COMPANY_INFO_API_URL } from "../constants";
 import { CompanyInfoResponse } from "types/CompanyInfoResponse";
-import { toCamelCase } from "lib/utils/transformationUtils";
+import { toCamelCase } from "../lib/utils/transformationUtils";
+import logger from "../lib/Logger";
 
 export class AccountUserCompanyServiceClient {
     client: AxiosInstance;
@@ -12,13 +13,13 @@ export class AccountUserCompanyServiceClient {
         });
     }
 
-    async getCompanyInfoForCompanyNumber (req: Request, companyNumber: string) {
+    async getCompanyInfoForCompanyNumber (companyNumber: string): Promise<CompanyInfoResponse | undefined> {
         try {
             const response = await this.client.get(`${COMPANY_INFO_API_URL}/${companyNumber}`);
             const companyInfoResponse: CompanyInfoResponse = toCamelCase(response.data);
             return companyInfoResponse;
-        } catch (error) {
-
+        } catch (error: any) {
+            logger.error(error);
         }
     }
 }
