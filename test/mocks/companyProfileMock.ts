a71e2fe8afa6b940a1f00091a87a8af8332d4ffa
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-01-26 15:29:05 +0000

    IDVA6-339 New tests and mocks added

--- test/mocks/companyProfileMock.ts
+++ test/mocks/companyProfileMock.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from "http-status-codes";
 
 jest.mock("../../src/services/companyProfileService");
 
-export const validCompanyProfile: CompanyProfile = {
+export const validActiveCompanyProfile: CompanyProfile = {
     accounts: {
         nextAccounts: {
             periodEndOn: "2019-10-10",
@@ -45,9 +45,49 @@ export const validCompanyProfile: CompanyProfile = {
     type: "ltd"
 };
 
+export const validDisolvedCompanyProfile: CompanyProfile = {
+    accounts: {
+        nextAccounts: {
+            periodEndOn: "2019-10-10",
+            periodStartOn: "2019-01-01"
+        },
+        nextDue: "2020-05-31",
+        overdue: false
+    },
+    companyName: "Test Company",
+    companyNumber: "12345678",
+    companyStatus: "dissolved",
+    companyStatusDetail: "company status detail",
+    confirmationStatement: {
+        lastMadeUpTo: "2019-04-30",
+        nextDue: "2020-04-30",
+        nextMadeUpTo: "2020-03-15",
+        overdue: false
+    },
+    dateOfCreation: "1972-06-22",
+    hasBeenLiquidated: false,
+    hasCharges: false,
+    hasInsolvencyHistory: false,
+    jurisdiction: "england-wales",
+    links: {},
+    registeredOfficeAddress: {
+        addressLineOne: "Line1",
+        addressLineTwo: "Line2",
+        careOf: "careOf",
+        country: "uk",
+        locality: "locality",
+        poBox: "123",
+        postalCode: "POST CODE",
+        premises: "premises",
+        region: "region"
+    },
+    sicCodes: ["123"],
+    type: "ltd"
+};
+
 export const validSDKResource: Resource<CompanyProfile> = {
     httpStatusCode: StatusCodes.OK,
-    resource: validCompanyProfile
+    resource: validActiveCompanyProfile
 };
 
 export const CompanyProfileErrorResponse: ApiErrorResponse = {
