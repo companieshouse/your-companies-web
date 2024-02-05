a0b28c455a125aff51dcd92ddb663927eb393353
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-02-05 11:27:34 +0000

    IDVA6-419 Unnecessary imports removed

--- src/routers/controllers/addCompanyController.ts
+++ src/routers/controllers/addCompanyController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { AddCompanyHandler } from "../handlers/yourCompanies/addCompany";
 import {
     ADD_COMPANY_PAGE_TEMPLATE,
--- src/routers/controllers/yourCompaniesController.ts
+++ src/routers/controllers/yourCompaniesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { YourCompaniesHandler } from "../handlers/yourCompanies/yourCompanies";
 import { YOUR_COMPANIES_PAGE_TEMPLATE } from "../../constants";
 
