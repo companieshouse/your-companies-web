66abd93fe32fc15be7a13439682a07ddc774d70f
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-02-05 10:20:31 +0000

    IDVA6-419 Authorisation check for company number changed from session to url param

--- src/middleware/company.authentication.ts
+++ src/middleware/company.authentication.ts
@@ -1,16 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { authMiddleware, AuthOptions } from "@companieshouse/web-security-node";
 import * as constants from "../constants";
-import { Session } from "@companieshouse/node-session-handler";
 
 // We are checking if the current user is authorised for a company / company number
-// based on the value stored in the session
-// This can be changed to a number passed in the url (req.params)
+// based on the value passed in the url (req.params)
 
 export const companyAuthenticationMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
-    const session: Session = req.session as Session;
-    const companyNumber: string | undefined = session?.getExtraData(constants.COMPANY_NUMBER);
+    const companyNumber: string | undefined = req.params[constants.COMPANY_NUMBER];
 
     const authMiddlewareConfig: AuthOptions = {
         chsWebUrl: constants.CHS_URL,
