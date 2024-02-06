2fee001be5eb648c669a98c834483ff364f634d9
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-02-06 16:50:26 +0000

    IDVA6-419 Hard coded strings replaced with a constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
 import { getEnvironmentValue } from "./lib/utils/environmentValue";
 
+// session.extra_data
+export const COMPANY_NUMBER = "companyNumber";
+export const COMPANY_PROFILE = "companyProfile";
+export const USER_EMAIL = "userEmail";
+
 // Paths to Nunjucks template files
 export const ERROR_400_TEMPLATE = "partials/error_400";
 export const YOUR_COMPANIES_TEMPLATE_FOLDER = "router_views/your_companies";
@@ -20,15 +25,15 @@ export const YOUR_COMPANIES_URL = "/";
 export const YOUR_COMPANIES_ADD_COMPANY_URL = `${LANDING_URL}${ADD_COMPANY_URL}`;
 export const CONFIRM_COMPANY_DETAILS_URL = "/confirm-company-details";
 export const YOUR_COMPANIES_CONFIRM_COMPANY_DETAILS_URL = `${LANDING_URL}${CONFIRM_COMPANY_DETAILS_URL}`;
-export const MANAGE_AUTHORISED_PEOPLE_URL = "/manage-authorised-people/:companyNumber";
+export const MANAGE_AUTHORISED_PEOPLE_URL = `/manage-authorised-people/:${COMPANY_NUMBER}`;
 export const YOUR_COMPANIES_MANAGE_AUTHORISED_PEOPLE_URL = `${LANDING_URL}${MANAGE_AUTHORISED_PEOPLE_URL}`;
 export const ADD_NEW_AUTHORISED_PERSON_URL = "/add-presenter";
 export const YOUR_COMPANIES_ADD_NEW_AUTHORISED_PERSON_URL = `${LANDING_URL}${ADD_NEW_AUTHORISED_PERSON_URL}`;
-export const CANCEL_PERSON_URL = "/cancel-person/:userEmail";
+export const CANCEL_PERSON_URL = `/cancel-person/:${USER_EMAIL}`;
 export const YOUR_COMPANIES_CANCEL_PERSON_URL = `${LANDING_URL}${CANCEL_PERSON_URL}`;
-export const MANAGE_AUTHORISED_PEOPLE_EMAIL_RESENT_URL = "/manage-authorised-people-email-resent/:userEmail";
+export const MANAGE_AUTHORISED_PEOPLE_EMAIL_RESENT_URL = `/manage-authorised-people-email-resent/:${USER_EMAIL}`;
 export const YOUR_COMPANIES_MANAGE_AUTHORISED_PEOPLE_EMAIL_RESENT_URL = `${LANDING_URL}${MANAGE_AUTHORISED_PEOPLE_EMAIL_RESENT_URL}`;
-export const AUTHENTICATION_CODE_REMOVE = "/authentication-code-remove/:userEmail";
+export const AUTHENTICATION_CODE_REMOVE = `/authentication-code-remove/:${USER_EMAIL}`;
 export const YOUR_COMPANIES_AUTHENTICATION_CODE_REMOVE = `${LANDING_URL}${AUTHENTICATION_CODE_REMOVE}`;
 
 // Error message keys
@@ -57,10 +62,6 @@ export const CHS_URL = getEnvironmentValue("CHS_URL");
 export const COOKIE_DOMAIN = getEnvironmentValue("COOKIE_DOMAIN");
 export const COOKIE_NAME = getEnvironmentValue("COOKIE_NAME");
 export const COOKIE_SECRET = getEnvironmentValue("COOKIE_SECRET");
-
-// session.extra_data
-export const COMPANY_NUMBER = "companyNumber";
-export const COMPANY_PROFILE = "companyProfile";
 
 // Booleans
 export const TRUE = "true";
