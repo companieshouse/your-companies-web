555671fbf8ae623d49d0062ac82777a57e0702f0
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-02-07 16:48:54 +0000

    IDVA6-419 New tests and mocks added

--- test/mocks/associations.mock.ts
+++ test/mocks/associations.mock.ts
@@ -21,6 +21,45 @@ export const userAssociations: Associations = {
     ]
 } as Associations;
 
-export const emptyUserAssociations: Associations = {
+export const companyAssociations: Associations = {
+    items: [
+        {
+            id: "1234567890",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY",
+            status: "Awaiting confirmation"
+        },
+        {
+            id: "2345678901",
+            userId: "jsldkfjsd",
+            userEmail: "john.smith@test.com",
+            displayName: "Not provided",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY",
+            status: "Confirmed"
+        },
+        {
+            id: "2345678901",
+            userId: "jsldkfjsd",
+            userEmail: "eva.brown@company.com",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY",
+            status: "Awaiting confirmation"
+        },
+        {
+            id: "2345678901",
+            userId: "jsldkfjsd",
+            userEmail: "mark.black@private.com",
+            displayName: "Mark Black",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY",
+            status: "Confirmed"
+        }
+    ]
+} as Associations;
+
+export const emptyAssociations: Associations = {
     items: []
 };
