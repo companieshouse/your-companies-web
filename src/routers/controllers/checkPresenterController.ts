01c9eaa0a0cc1f9048325ee381d9084f19dbd352
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-02-15 22:05:17 +0000

    mock add user

--- src/routers/controllers/checkPresenterController.ts
+++ src/routers/controllers/checkPresenterController.ts
@@ -6,17 +6,45 @@ import { getCompanyProfile } from "../../services/companyProfileService";
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
 import { getUrlWithCompanyNumber } from "../../lib/utils/urlUtils";
 import { Session } from "@companieshouse/node-session-handler";
+import { polishBrewItems, britishAirwaysItems } from "../../services/userCompanyAssociationService";
 
 export const checkPresenterController = async (req: Request, res: Response) => {
     const company: CompanyProfile = await getCompanyProfile(req.params[constants.COMPANY_NUMBER] as string);
+    const session: Session = req.session as Session;
+    const emailAddress: string | undefined = session?.getExtraData(constants.AUTHORISED_PERSON_EMAIL);
+
     if (req.method === constants.POST) {
+        if (company.companyNumber === "NI038379" && emailAddress) {
+            polishBrewItems.items.push(
+                {
+                    id: "",
+                    userId: "",
+                    userEmail: emailAddress,
+                    companyNumber: "NI038379",
+                    companyName: "THE POLISH BREWERY",
+                    status: "Awaiting confirmation",
+                    displayName: ""
+                }
+            );
+        }
+        if (company.companyName === "01777777" && emailAddress) {
+            polishBrewItems.items.push(
+                {
+                    id: "",
+                    userId: "",
+                    userEmail: emailAddress,
+                    displayName: "",
+                    companyNumber: "01777777",
+                    companyName: "BRITISH AIRWAYS PLC",
+                    status: "Awaiting confirmation"
+                }
+            );
+        }
         return res.redirect(constants.YOUR_COMPANIES_MANAGE_AUTHORISED_PEOPLE_URL.replace(
             `:${constants.COMPANY_NUMBER}`,
             company.companyNumber
         ));
     }
-    const session: Session = req.session as Session;
-    const emailAddress: string | undefined = session?.getExtraData(constants.AUTHORISED_PERSON_EMAIL);
 
     const viewData = {
         lang: getTranslationsForView(req.t, pages.CHECK_PRESENTER),
--- test/src/routers/controllers/checkPresenterController.test.ts
+++ test/src/routers/controllers/checkPresenterController.test.ts
@@ -2,15 +2,22 @@ import mocks from "../../../mocks/all.middleware.mock";
 import app from "../../../../src/app";
 import supertest from "supertest";
 import { getCompanyProfile } from "../../../../src/services/companyProfileService";
-import { isEmailAuthorised } from "../../../../src/services/userCompanyAssociationService";
 import { validActiveCompanyProfile } from "../../../mocks/companyProfile.mock";
 import * as constants from "../../../../src/constants";
+import { Session } from "@companieshouse/node-session-handler";
+import { isCompanyAssociatedWithUser } from "../../../../src/services/userCompanyAssociationService";
+import { NextFunction, Request, Response } from "express";
 
 jest.mock("../../../../src/services/companyProfileService");
 jest.mock("../../../../src/services/userCompanyAssociationService");
+const session: Session = new Session();
+
+mocks.mockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+    req.session = session;
+    return next();
+});
 
 const mockGetCompanyProfile = getCompanyProfile as jest.Mock;
-const mockIsEmailAuthorised = isEmailAuthorised as jest.Mock;
 
 const router = supertest(app);
 const en = require("../../../../src/locales/en/translation/add-presenter-check-details.json");
@@ -52,7 +59,7 @@ describe(`POST ${url}`, () => {
 
     beforeEach(() => {
         jest.clearAllMocks();
-        mockGetCompanyProfile.mockResolvedValueOnce(validActiveCompanyProfile);
+        mockGetCompanyProfile.mockResolvedValueOnce({ ...validActiveCompanyProfile, companyNumber: "NI038379" });
     });
 
     it("should check session, company and user auth before routing to controller", async () => {
@@ -62,9 +69,14 @@ describe(`POST ${url}`, () => {
         expect(mocks.mockCompanyAuthenticationMiddleware).toHaveBeenCalled();
     });
 
+    it("should add user", async () => {
+        await router.post(url);
+        session.data.extra_data.authorisedPersonEmail = "bob@bob.com";
+        expect(isCompanyAssociatedWithUser("NI038379", "bob@bob.com")).toBeTruthy;
+    });
+
     it("should redirect to the manage authorised people page", async () => {
-        mockIsEmailAuthorised.mockResolvedValueOnce(false);
-        const response = await router.post(url).send({ email: "bob@bob.com" });
+        const response = await router.post(url);
         expect(response.status).toEqual(302);
         expect(response.header.location).toEqual(constants.YOUR_COMPANIES_MANAGE_AUTHORISED_PEOPLE_URL.replace(
             `:${constants.COMPANY_NUMBER}`, "12345678"
