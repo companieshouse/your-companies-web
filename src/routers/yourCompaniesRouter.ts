7d5ac98b655d6d3d53277ee41c0f63eb4c6f5b3e
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-02-21 09:23:52 +0000

    adjust spacing

--- src/routers/yourCompaniesRouter.ts
+++ src/routers/yourCompaniesRouter.ts
@@ -16,33 +16,25 @@ const router: Router = Router();
 router.get(constants.YOUR_COMPANIES_URL, yourCompaniesControllerGet);
 
 router.get(constants.MANAGE_AUTHORISED_PEOPLE_URL, manageAuthorisedPeopleControllerGet);
-
 router.get(constants.MANAGE_AUTHORISED_PEOPLE_CONFIRMATION_CANCEL_PERSON_URL, manageAuthorisedPeopleControllerGet);
+router.get(constants.AUTHORISED_PERSON_ADDED_URL, isComingFromCheckEmailPage, manageAuthorisedPeopleControllerGet);
 
 router.get(constants.ADD_COMPANY_URL, addCompanyControllerGet);
-
 router.post(constants.ADD_COMPANY_URL, addCompanyControllerPost);
 
 router.get(constants.CANCEL_PERSON_URL, cancelPersonControllerGet);
-
 router.post(constants.CANCEL_PERSON_URL, cancelPersonControllerPost);
 
 router.get(constants.CONFIRM_COMPANY_DETAILS_URL, confirmCompanyControllerGet);
-
 router.post(constants.CONFIRM_COMPANY_DETAILS_URL, confirmCompanyControllerPost);
 
 router.get(constants.CREATE_TRANSACTION_PATH, createTransactionControllerGet);
-
 router.get(constants.COMPANY_ADDED_SUCCESS_URL, companyAddedControllerGet);
 
 router.get(constants.ADD_PRESENTER_URL, addPresenterController);
-
 router.post(constants.ADD_PRESENTER_URL, addPresenterController);
 
 router.get(constants.CHECK_PRESENTER_URL, checkPresenterController);
-
 router.post(constants.CHECK_PRESENTER_URL, checkPresenterController);
-
-router.get(constants.AUTHORISED_PERSON_ADDED_URL, isComingFromCheckEmailPage, manageAuthorisedPeopleControllerGet);
 
 export default router;
