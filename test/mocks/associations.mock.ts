83d72dc1bf1b3075fe267ac023d0d3c7e055e304
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-02-28 15:35:24 +0000

    IDVA6-531 Add company tests separated to their own file, new tests for invitations banner added and mocks updated

--- test/mocks/associations.mock.ts
+++ test/mocks/associations.mock.ts
@@ -21,6 +21,26 @@ export const userAssociations: Associations = {
     ]
 } as Associations;
 
+export const userAssociationsWithNumberOfInvitations: Associations = {
+    items: [
+        {
+            id: "1234567890",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY"
+        },
+        {
+            id: "2345678901",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "01777777",
+            companyName: "BRITISH AIRWAYS PLC"
+        }
+    ],
+    totalResults: 3
+} as Associations;
+
 export const companyAssociations: Associations = {
     items: [
         {
