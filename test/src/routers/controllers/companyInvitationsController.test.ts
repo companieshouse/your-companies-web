e14d2f206be9c88c249e69f83bb71ae08b054c29
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-02-29 09:21:40 +0000

    add unit test

---
+++ test/src/routers/controllers/companyInvitationsController.test.ts
@@ -0,0 +1,140 @@
+import mocks from "../../../mocks/all.middleware.mock";
+import app from "../../../../src/app";
+import supertest from "supertest";
+import * as en from "../../../../src/locales/en/translation/company-invitations.json";
+import * as cy from "../../../../src/locales/cy/translation/company-invitations.json";
+import { getUserAssociations } from "../../../../src/services/userCompanyAssociationService";
+import { getUserRecord } from "../../../../src/services/userService";
+import { Associations } from "../../../../src/types/associations";
+jest.mock("../../../../src/services/userCompanyAssociationService");
+jest.mock("../../../../src/services/userService");
+
+const mockGetUserAssociations = getUserAssociations as jest.Mock;
+const mockGetUserRecord = getUserRecord as jest.Mock;
+
+const router = supertest(app);
+const url = "/your-companies/company-invitations"
+
+const associations: Associations = {
+    items: [
+        {
+            id: "1234567890",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "NI038379",
+            companyName: "THE POLISH BREWERY",
+            status: "confirmed",
+            invitations:
+            [
+                {
+                    invited_by: "123454321",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                }
+            ]
+        },
+        {
+            id: "2345678901",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "01777777",
+            companyName: "BRITISH AIRWAYS PLC",
+            status: "confirmed",
+            invitations:
+            [
+                {
+                    invited_by: "123454321",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                }
+            ]
+        },
+        {
+            id: "44345677554",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "10866549",
+            companyName: "ANDROID TECHNOLOGY LTD",
+            status: "awaiting-approval",
+            invitations:
+            [
+                {
+                    invited_by: "1122334455",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                },
+                {
+                    invited_by: "75853993475",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                }
+            ]
+        },
+        {
+            id: "234322344",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "08449801",
+            companyName: "BROWN AND SALTER LIMITED",
+            status: "awaiting-approval",
+            invitations:
+            [
+                {
+                    invited_by: "5544332211",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                }
+            ]
+
+        },
+        {
+            id: "6654463562412",
+            userId: "qwertyiop",
+            userEmail: "demo@ch.gov.uk",
+            companyNumber: "18882777",
+            companyName: "FLOWERS LIMITED",
+            status: "awaiting-approval",
+            invitations:
+            [
+                {
+                    invited_by: "76896789",
+                    invited_at: "2022-04-05T11:41:09.568+00:00 UTC"
+                }
+            ]
+
+        }
+    ]
+} as Associations;
+
+describe(`GET ${url}`, () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockGetUserAssociations.mockResolvedValueOnce(associations);
+        mockGetUserRecord.mockResolvedValue({
+            surname: "",
+            email: "another.email@acme.com",
+            user_id: "75853993475",
+            roles: ""
+        });
+    });
+    it("should check session, company and user auth before returning the page", async () => {
+        await router.get(url);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
+    it("should return expected English content if no language selected", async () => {
+        const response = await router.get(`${url}`);
+        expect(response.text).toContain(en.h1);
+        expect(response.text).toContain(en.company_name);
+        expect(response.text).toContain(en.company_number);
+    });
+    it("should return expected Welsh content if Welsh is selected", async () => {
+        const response = await router.get(`${url}?lang=cy`);
+        expect(response.text).toContain(cy.h1);
+        expect(response.text).toContain(cy.company_name);
+        expect(response.text).toContain(cy.company_number);
+    });
+    it("should return user email who created the invitation", async () => {
+        const response = await router.get(`${url}?lang=cy`);
+        const expectedEmail = "another.email@acme.com"
+        const expectedCompanyNumber = "08449801"
+        expect(response.text).toContain(expectedCompanyNumber);
+        expect(response.text).toContain(expectedEmail);
+    });
+});
