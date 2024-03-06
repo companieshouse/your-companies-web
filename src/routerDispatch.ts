bdaa49e29a6ffac5d1c15363391eae8e79c39bf5
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-03-06 11:23:00 +0000

    IDVA6-532 Router renamed in import

--- src/routerDispatch.ts
+++ src/routerDispatch.ts
@@ -1,10 +1,10 @@
 // Do Router dispatch here, i.e. map incoming routes to appropriate router
 import { Application, Request, Response } from "express";
-import yourCompaniesRouter from "./routers/router";
+import router from "./routers/router";
 import { ERROR_400_TEMPLATE, LANDING_URL } from "./constants";
 
 const routerDispatch = (app: Application) => {
-    app.use(LANDING_URL, yourCompaniesRouter);
+    app.use(LANDING_URL, router);
     app.use("*", (req: Request, res: Response) => {
         return res.status(404).render(ERROR_400_TEMPLATE);
     });
