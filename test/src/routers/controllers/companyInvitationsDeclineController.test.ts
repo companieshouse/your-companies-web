8e27be87470e3a0b45661365a58fb7e46041a5f1
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-03-07 16:31:15 +0000

    IDVA6-534 - updating unit tests

--- test/src/routers/controllers/companyInvitationsDeclineController.test.ts
+++ test/src/routers/controllers/companyInvitationsDeclineController.test.ts
@@ -3,14 +3,14 @@ import app from "../../../../src/app";
 import supertest from "supertest";
 import { Session } from "@companieshouse/node-session-handler";
 import { NextFunction, Request, Response } from "express";
-import { COMPANY_NAME } from "../../../../src/constants";
 import * as en from "../../../../src/locales/en/translation/company-invitations-decline.json";
 import * as cy from "../../../../src/locales/cy/translation/company-invitations-decline.json";
 
 const router = supertest(app);
 const session: Session = new Session();
 const associationId = "123456";
-const url = `/your-companies/company-invitations-decline/${associationId}`;
+const companyName = "Doughnuts Limited";
+const url = `/your-companies/company-invitations-decline/${associationId}?companyName=${companyName}`;
 
 mocks.mockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
     req.session = session;
@@ -33,35 +33,29 @@ describe("GET /your-companies/companies-invitations-decline/:associationId", () 
         await router.get(url).expect(200);
     });
 
-    // it("should return expected English content if language version set to English", async () => {
-    //     // Given
-    //     const langVersion = "&lang=en";
-    //     const expectedCompanyName = "Doughnuts Limited";
-    //     session.setExtraData(COMPANY_NAME, expectedCompanyName);
-    //     // When
-    //     const response = await router.get(`${url}${langVersion}`);
-    //     // Then
-    //     expect(response.text).toContain(en.invitation_declined);
-    //     expect(response.text).toContain(`${en.you_have_declined_to_be_digitally_authorised}${expectedCompanyName}`);
-    //     expect(response.text).toContain(en.what_happens_now_youve_declined);
-    //     expect(response.text).toContain(en.weve_sent_an_email);
-    //     expect(response.text).toContain(en.view_your_companies);
-    // });
+    it("should return expected English content if language version set to English", async () => {
+        // Given
+        const langVersion = "&lang=en";
+        // When
+        const response = await router.get(`${url}${langVersion}`);
+        // Then
+        expect(response.text).toContain(en.invitation_declined);
+        expect(response.text).toContain(`${en.you_have_declined_to_be_digitally_authorised}${companyName}`);
+        expect(response.text).toContain(en.what_happens_now_youve_declined);
+        expect(response.text).toContain(en.weve_sent_an_email);
+        expect(response.text).toContain(en.view_your_companies);
+    });
 
-    // it("should return expected Welsh content if language version set to Welsh", async () => {
-    //     // Given
-    //     const langVersion = "&lang=cy";
-    //     const expectedCompanyName = "Doughnuts Limited";
-    //     session.setExtraData(COMPANY_NAME, expectedCompanyName);
-    //     // When
-    //     const response = await router.get(`${url}${langVersion}`);
-    //     // Then
-    //     expect(response.text).toContain(cy.invitation_declined);
-    //     // expect(response.text).toContain(`${cy.you_have_declined_to_be_digitally_authorised}${expectedCompanyName}`);
-    //     expect(response.text).toContain(cy.you_have_declined_to_be_digitally_authorised);
-    //     expect(response.text).toContain(expectedCompanyName);
-    //     expect(response.text).toContain(cy.what_happens_now_youve_declined);
-    //     expect(response.text).toContain(cy.weve_sent_an_email);
-    //     expect(response.text).toContain(cy.view_your_companies);
-    // });
+    it("should return expected Welsh content if language version set to Welsh", async () => {
+        // Given
+        const langVersion = "&lang=cy";
+        // When
+        const response = await router.get(`${url}${langVersion}`);
+        // Then
+        expect(response.text).toContain(cy.invitation_declined);
+        expect(response.text).toContain(`${cy.you_have_declined_to_be_digitally_authorised}${companyName}`);
+        expect(response.text).toContain(cy.what_happens_now_youve_declined);
+        expect(response.text).toContain(cy.weve_sent_an_email);
+        expect(response.text).toContain(cy.view_your_companies);
+    });
 });
