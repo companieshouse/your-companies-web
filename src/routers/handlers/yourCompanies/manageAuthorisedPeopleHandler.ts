890bc3e964656c4f752888aaf6e034ce8dd8d8c6
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-03-08 11:30:14 +0000

    IDVA6-368 Code smell listed by SonarQube fixed

--- src/routers/handlers/yourCompanies/manageAuthorisedPeopleHandler.ts
+++ src/routers/handlers/yourCompanies/manageAuthorisedPeopleHandler.ts
@@ -24,35 +24,14 @@ export class ManageAuthorisedPeopleHandler extends GenericHandler {
 
         if (cancellation && req.originalUrl.includes(constants.CONFIRMATION_CANCEL_PERSON_URL)) {
             deleteExtraData(req.session, constants.REMOVE_PERSON);
-            if (cancellation.cancelPerson === constants.YES) {
-                const isUserRemovedFromCompanyAssociations = (await removeUserFromCompanyAssociations(cancellation.userEmail, cancellation.companyNumber)) === constants.USER_REMOVED_FROM_COMPANY_ASSOCIATIONS;
-                if (isUserRemovedFromCompanyAssociations) {
-                    this.viewData.cancelledPerson = cancellation.userEmail;
-                }
-            }
+            await this.handleCancellation(cancellation);
         } else if (removal && req.originalUrl.includes(constants.CONFIRMATION_PERSON_REMOVED_URL)) {
             deleteExtraData(req.session, constants.CANCEL_PERSON);
-            if (removal.removePerson === constants.CONFIRM) {
-                const isUserRemovedFromCompanyAssociations = (await removeUserFromCompanyAssociations(removal.userEmail, removal.companyNumber)) === constants.USER_REMOVED_FROM_COMPANY_ASSOCIATIONS;
-                if (isUserRemovedFromCompanyAssociations) {
-                    this.viewData.removedPerson = removal.userName ? removal.userName : removal.userEmail;
-                    this.viewData.changeCompanyAuthCodeUrl = "https://www.gov.uk/guidance/company-authentication-codes-for-online-filing#change-or-cancel-your-code";
-                }
-            }
-        }
-        const authorisedPerson = getExtraData(req.session, constants.AUTHORISED_PERSON);
-
-        if (authorisedPerson && req.originalUrl.includes("/confirmation-person-added")) {
-            this.viewData.authorisedPersonSuccess = true;
-            this.viewData.authorisedPersonEmailAddress = authorisedPerson.authorisedPersonEmailAddress;
-            this.viewData.authorisedPersonCompanyName = authorisedPerson.authorisedPersonCompanyName;
+            await this.handleRemoval(removal);
         }
 
-        const resentSuccessEmail = getExtraData(req.session, constants.RESENT_SUCCESS_EMAIL);
-        if (resentSuccessEmail && req.originalUrl.includes(constants.AUTHORISATION_EMAIL_RESENT_URL)) {
-            this.viewData.showEmailResentSuccess = true;
-            this.viewData.resentSuccessEmail = resentSuccessEmail;
-        }
+        this.handleConfirmationPersonAdded(req);
+        this.handleResentSuccessEmail(req);
 
         const companyAssociations: Associations = await getCompanyAssociations(companyNumber, cancellation || removal);
         this.viewData.companyAssociations = companyAssociations;
@@ -72,5 +51,41 @@ export class ManageAuthorisedPeopleHandler extends GenericHandler {
             resendEmailUrl: constants.YOUR_COMPANIES_MANAGE_AUTHORISED_PEOPLE_EMAIL_RESENT_URL,
             removeUrl: constants.YOUR_COMPANIES_AUTHENTICATION_CODE_REMOVE_URL
         };
+    }
+
+    private async handleCancellation (cancellation: Cancellation) {
+        if (cancellation.cancelPerson === constants.YES) {
+            const isUserRemovedFromCompanyAssociations = (await removeUserFromCompanyAssociations(cancellation.userEmail, cancellation.companyNumber)) === constants.USER_REMOVED_FROM_COMPANY_ASSOCIATIONS;
+            if (isUserRemovedFromCompanyAssociations) {
+                this.viewData.cancelledPerson = cancellation.userEmail;
+            }
+        }
+    }
+
+    private async handleRemoval (removal: Removal) {
+        if (removal.removePerson === constants.CONFIRM) {
+            const isUserRemovedFromCompanyAssociations = (await removeUserFromCompanyAssociations(removal.userEmail, removal.companyNumber)) === constants.USER_REMOVED_FROM_COMPANY_ASSOCIATIONS;
+            if (isUserRemovedFromCompanyAssociations) {
+                this.viewData.removedPerson = removal.userName ? removal.userName : removal.userEmail;
+                this.viewData.changeCompanyAuthCodeUrl = "https://www.gov.uk/guidance/company-authentication-codes-for-online-filing#change-or-cancel-your-code";
+            }
+        }
+    }
+
+    private handleResentSuccessEmail (req: Request) {
+        const resentSuccessEmail = getExtraData(req.session, constants.RESENT_SUCCESS_EMAIL);
+        if (resentSuccessEmail && req.originalUrl.includes(constants.AUTHORISATION_EMAIL_RESENT_URL)) {
+            this.viewData.showEmailResentSuccess = true;
+            this.viewData.resentSuccessEmail = resentSuccessEmail;
+        }
+    }
+
+    private handleConfirmationPersonAdded (req: Request) {
+        const authorisedPerson = getExtraData(req.session, constants.AUTHORISED_PERSON);
+        if (authorisedPerson && req.originalUrl.includes("/confirmation-person-added")) {
+            this.viewData.authorisedPersonSuccess = true;
+            this.viewData.authorisedPersonEmailAddress = authorisedPerson.authorisedPersonEmailAddress;
+            this.viewData.authorisedPersonCompanyName = authorisedPerson.authorisedPersonCompanyName;
+        }
     }
 }
