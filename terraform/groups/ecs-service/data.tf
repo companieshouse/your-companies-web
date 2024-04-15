209b8d2d8485d32cab01a3bd3c642ea7bb39627a
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2024-04-15 17:48:43 +0100

    Changing the LB

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -32,12 +32,12 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
   name = "${local.name_prefix}-ecs-task-execution-role"
 }
 
-data "aws_lb" "service_lb" {
+data "aws_lb" "identity_lb" {
   name = "${var.environment}-chs-chgovuk"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
-  load_balancer_arn = data.aws_lb.service_lb.arn
+  load_balancer_arn = data.aws_lb.identity_lb.arn
   port = 443
 }
 
