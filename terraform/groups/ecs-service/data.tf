a12712630599d785d6ec8ed639ff4a289367aec4
sadiomowo-ch <sadiomowo@companieshouse.gov.uk>
2024-04-15 17:35:03 +0100

    Changing the LB

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -33,7 +33,7 @@ data "aws_iam_role" "ecs_cluster_iam_role" {
 }
 
 data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-accountchgovuk"
+  name = "${var.environment}-chs-chgovuk"
 }
 
 data "aws_lb_listener" "service_lb_listener" {
