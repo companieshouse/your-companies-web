c1e20509df5e5dac87c1c27b173f171bba1fb32f
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-04-15 15:52:41 +0100

    IDVA6-709 - changing previous page in companyAddedController

--- src/routers/controllers/companyAddedController.ts
+++ src/routers/controllers/companyAddedController.ts
@@ -9,7 +9,7 @@ export const companyAddedControllerGet = async (req: Request, res: Response): Pr
     const referrer :string|undefined = req.get("Referrer");
     const companyDetailsIndicator = getExtraData(req.session, constants.CONFIRM_COMPANY_DETAILS_INDICATOR);
 
-    if (redirectPage(referrer, constants.CONFIRM_COMPANY_DETAILS_URL, constants.COMPANY_ADDED_SUCCESS_URL, companyDetailsIndicator)) {
+    if (redirectPage(referrer, constants.YOUR_COMPANIES_CONFIRM_COMPANY_DETAILS_URL, constants.COMPANY_ADDED_SUCCESS_URL, companyDetailsIndicator)) {
         res.redirect(constants.LANDING_URL);
     } else {
 
