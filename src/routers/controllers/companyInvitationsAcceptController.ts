8e91d5d050bc91b3c361c17c97d40488780e195c
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-04-24 14:27:35 +0100

    IDVA6-709 - clearing up code smells

--- src/routers/controllers/companyInvitationsAcceptController.ts
+++ src/routers/controllers/companyInvitationsAcceptController.ts
@@ -7,7 +7,7 @@ import { getExtraData } from "../../lib/utils/sessionUtils";
 export const companyInvitationsAcceptControllerGet = async (req: Request, res: Response): Promise<void> => {
     const referrer :string|undefined = req.get("Referrer");
     const pageIndicator = getExtraData(req.session, constants.MANAGE_AUTHORISED_PEOPLE_INDICATOR);
-    const associationId = req.params[constants.ASSOCIATIONS_ID] as string;
+    const associationId = req.params[constants.ASSOCIATIONS_ID];
     const companyName = req.query[constants.COMPANY_NAME] as string;
     const hrefB = `${constants.YOUR_COMPANIES_COMPANY_INVITATIONS_ACCEPT_URL.replace(":associationId", associationId)}?${constants.COMPANY_NAME}=${companyName.replace(/ /g, "+")}`;
 
--- src/routers/controllers/companyInvitationsDeclineController.ts
+++ src/routers/controllers/companyInvitationsDeclineController.ts
@@ -7,7 +7,7 @@ import { redirectPage } from "../../lib/utils/referrerUtils";
 export const companyInvitationsDeclineControllerGet = async (req: Request, res: Response): Promise<void> => {
     const referrer :string|undefined = req.get("Referrer");
     const pageIndicator = getExtraData(req.session, constants.MANAGE_AUTHORISED_PEOPLE_INDICATOR);
-    const associationId = req.params[constants.ASSOCIATIONS_ID] as string;
+    const associationId = req.params[constants.ASSOCIATIONS_ID];
     const companyName = req.query[constants.COMPANY_NAME] as string;
     const hrefB = `${constants.YOUR_COMPANIES_COMPANY_INVITATIONS_DECLINE_URL.replace(":associationId", associationId)}?${constants.COMPANY_NAME}=${companyName.replace(/ /g, "+")}`;
 
