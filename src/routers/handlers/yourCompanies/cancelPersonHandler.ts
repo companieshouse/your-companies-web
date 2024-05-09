2b4f2febe4e8cb90d565d43811fec26d095d44b3
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-05-09 16:59:30 +0100

    IDVA6-997 Code refactored to remove imports of individual constants

--- src/routers/handlers/yourCompanies/cancelPersonHandler.ts
+++ src/routers/handlers/yourCompanies/cancelPersonHandler.ts
@@ -1,15 +1,6 @@
 import { Request, Response } from "express";
 import { GenericHandler } from "../genericHandler";
-import {
-    CANCEL_PERSON,
-    CANCEL_PERSON_PAGE,
-    COMPANY_NAME,
-    COMPANY_NUMBER,
-    POST,
-    REFERER_URL,
-    SELECT_YES_IF_YOU_WANT_TO_CANCEL_AUTHORISATION,
-    USER_EMAIL
-} from "../../../constants";
+import * as constants from "../../../constants";
 import { getTranslationsForView } from "../../../lib/utils/translations";
 import { getExtraData, setExtraData } from "../../../lib/utils/sessionUtils";
 import { Cancellation } from "../../../types/cancellation";
@@ -18,33 +9,33 @@ import { ViewData } from "../../../types/util-types";
 export class CancelPersonHandler extends GenericHandler {
     async execute (req: Request, res: Response, method: string): Promise<ViewData> {
         this.viewData = this.getViewData(req);
-        if (method === POST) {
+        if (method === constants.POST) {
             const payload = Object.assign({}, req.body);
             if (!payload.cancelPerson) {
                 this.viewData.errors = {
                     cancelPerson: {
-                        text: SELECT_YES_IF_YOU_WANT_TO_CANCEL_AUTHORISATION
+                        text: constants.SELECT_YES_IF_YOU_WANT_TO_CANCEL_AUTHORISATION
                     }
                 };
             } else {
                 const cancellation: Cancellation = {
                     cancelPerson: payload.cancelPerson,
-                    userEmail: req.params[USER_EMAIL],
-                    companyNumber: getExtraData(req.session, COMPANY_NUMBER)
+                    userEmail: req.params[constants.USER_EMAIL],
+                    companyNumber: getExtraData(req.session, constants.COMPANY_NUMBER)
                 };
-                setExtraData(req.session, CANCEL_PERSON, cancellation);
+                setExtraData(req.session, constants.CANCEL_PERSON, cancellation);
             }
         }
         return Promise.resolve(this.viewData);
     }
 
     private getViewData (req: Request): ViewData {
-        const lang = getTranslationsForView(req.t, CANCEL_PERSON_PAGE);
+        const lang = getTranslationsForView(req.t, constants.CANCEL_PERSON_PAGE);
         return {
             lang: lang,
-            backLinkHref: getExtraData(req.session, REFERER_URL),
-            companyName: getExtraData(req.session, COMPANY_NAME),
-            userEmail: req.params[USER_EMAIL],
+            backLinkHref: getExtraData(req.session, constants.REFERER_URL),
+            companyName: getExtraData(req.session, constants.COMPANY_NAME),
+            userEmail: req.params[constants.USER_EMAIL],
             buttonHref: req.originalUrl
         };
     }
