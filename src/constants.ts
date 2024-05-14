b718e454dd4894f787d78653a167a27b788b5ce1
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-05-14 16:40:54 +0100

    IDVA6-853 Matomo tags added to remove authorised person page

--- src/constants.ts
+++ src/constants.ts
@@ -153,6 +153,8 @@ export const MATOMO_CANCEL_AUTHORISED_USER_LINK = "Cancel Authorised User Link";
 export const MATOMO_RESEND_AUTHORISED_USER_EMAIL_LINK = "Resend Authorised User Email Link";
 export const MATOMO_CONFIRM_AND_SEND_EMAIL_BUTTON = "Confirm and Send Email Button";
 export const MATOMO_CHANGE_LINK = "Change Link";
+export const MATOMO_REMOVE_AUTHORISATION_BUTTON = "Remove Authorisation Button";
+export const MATOMO_CANCEL_LINK = "Cancel Link";
 
 // various
 export const NOT_PROVIDED = "Not provided";
