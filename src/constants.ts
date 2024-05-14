e060195fced80de8000a59ee90f8576caf011109
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-05-14 10:37:52 +0100

    IDVA6-848 Matomo tags added to confirmation company added page

--- src/constants.ts
+++ src/constants.ts
@@ -146,6 +146,7 @@ export const MATOMO_VIEW_AND_MANAGE_LINK = "View and Manage Link";
 export const MATOMO_CONTINUE_BUTTON = "Continue Button";
 export const MATOMO_CONFIRM_AND_CONTINUE_BUTTON = "Confirm and Continue Button";
 export const MATOMO_CHOOSE_DIFFERENT_COMPANY_LINK = "Choose a Different Company Link";
+export const MATOMO_VIEW_YOUR_COMPANIES_LINK = "View Your Companies Link";
 
 // various
 export const NOT_PROVIDED = "Not provided";
