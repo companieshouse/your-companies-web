7752d8d49d3adf6ddef4f7b010310954a76c95b3
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-05-20 16:18:17 +0100

    Adding a description for redirectPage documentation

--- src/lib/utils/referrerUtils.ts
+++ src/lib/utils/referrerUtils.ts
@@ -1,8 +1,11 @@
 import logger from "../Logger";
 
 /**
+ * This function checks url parameters predefined in page handlers to see if a user has travelled to their current page via the expected route.
+ * This is to prevent users bookmarking and accessing pages mid-way through the user journey, or urls that may no longer exist.
  *
  * @param referrer - redirect url - the specific url prior to the url the user is currently on
+ *                 - is undefined if a user has bookmarked the page they wish to access
  * @param hrefA - expected previous page url - compared with redirect parameter to confirm if user has travelled from the page prior to the current page in the journey
  * @param hrefB - expected current page url - compared with redirect parameter to account for whether user has changed language on current page, changing the url
  * @param pageIndicator - for when the authentication system causes referrer paramater to be 'undefined'
