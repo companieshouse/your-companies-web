5413c5d116db59bb6419bf7d29fbac1aced9908f
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-05-30 21:22:23 +0100

    add pagination http request tests

--- test/src/routers/controllers/manageAuthorisedPeopleController.pagination.test.ts
+++ test/src/routers/controllers/manageAuthorisedPeopleController.pagination.test.ts
@@ -1,10 +1,8 @@
 import mocks from "../../../mocks/all.middleware.mock";
-import { companyAssociations } from "../../../mocks/associations.mock";
+import { companyAssociationsPage2, companyAssociationsPage1 } from "../../../mocks/associations.mock";
 import app from "../../../../src/app";
 import * as associationsService from "../../../../src/services/associationsService";
 import supertest from "supertest";
-import * as en from "../../../../src/locales/en/translation/manage-authorised-people.json";
-import * as cy from "../../../../src/locales/cy/translation/manage-authorised-people.json";
 import * as enCommon from "../../../../src/locales/en/translation/common.json";
 import * as cyCommon from "../../../../src/locales/cy/translation/common.json";
 
@@ -34,66 +32,60 @@ describe("GET /your-companies/manage-authorised-people/:companyNumber", () => {
     });
 
     it("should check session and auth before returning the /your-companies/manage-authorised-people/NI038379 page", async () => {
-        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociations));
+        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociationsPage1));
         await router.get(url);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
     });
 
-    it("should return status 200", async () => {
+    it("should display all 15 associations for page 1 with a next button", async () => {
         // Given
-        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociations));
-        // When
-        const response = await router.get(url);
-        // Then
-        expect(response.status).toEqual(200);
-    });
-
-    it("should return expected English content if language version set to English", async () => {
-        // Given
-        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociations));
+        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociationsPage1));
         // When
         const response = await router.get(`${url}`);
         // Then
-        expect(response.text).toContain(`${companyAssociations.items[0].companyName} (${companyAssociations.items[0].companyNumber})`);
-        expect(response.text).toContain(en.people_digitally_authorised_to_file_online);
-        expect(response.text).toContain(en.anyone_with_access_to_the_current_authentication);
-        expect(response.text).toContain(en.add_new_authorised_person);
-        expect(response.text).toContain(en.details_of_authorised_people);
-        expect(response.text).toContain(en.email_address);
-        expect(response.text).toContain(en.name);
-        expect(response.text).toContain(en.status);
-        expect(response.text).toContain(en.remove);
-        expect(response.text).toContain(enCommon.go_back_to_your_companies);
-        expect(response.text).not.toContain(enCommon.success);
-        expect(response.text).not.toContain(en.digital_authorisation_cancelled);
-        expect(response.text).toContain(companyAssociations.items[0].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[1].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[2].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[3].userEmail + "</th>");
+        companyAssociationsPage1.items.forEach((association) => {
+            expect(response.text).toContain(association.userEmail + "</th>");
+        });
+        expect(response.text).toContain(enCommon.next);
+        expect(response.text).not.toContain(enCommon.previous);
     });
 
-    it("should return expected Welsh content if language version set to Welsh", async () => {
+    it("should display the assocations for the last paginated page", async () => {
         // Given
-        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociations));
+        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(companyAssociationsPage2));
         // When
-        const response = await router.get(`${url}?lang=cy`);
+        const response = await router.get(`${url}?page=2`);
         // Then
-        expect(response.text).toContain(`${companyAssociations.items[0].companyName} (${companyAssociations.items[0].companyNumber})`);
-        expect(response.text).toContain(cy.people_digitally_authorised_to_file_online);
-        expect(response.text).toContain(cy.anyone_with_access_to_the_current_authentication);
-        expect(response.text).toContain(cy.add_new_authorised_person);
-        expect(response.text).toContain(cy.details_of_authorised_people);
-        expect(response.text).toContain(cy.email_address);
-        expect(response.text).toContain(cy.name);
-        expect(response.text).toContain(cy.status);
-        expect(response.text).toContain(cy.remove);
-        expect(response.text).toContain(cyCommon.go_back_to_your_companies);
-        expect(response.text).not.toContain(cyCommon.success);
-        expect(response.text).not.toContain(cy.digital_authorisation_cancelled);
-        expect(response.text).toContain(companyAssociations.items[0].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[1].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[2].userEmail + "</th>");
-        expect(response.text).toContain(companyAssociations.items[3].userEmail + "</th>");
+        expect(response.text).toContain(companyAssociationsPage2.items[0].userEmail + "</th>");
+        expect(response.text).toContain(companyAssociationsPage2.items[1].userEmail + "</th>");
+        expect(response.text).not.toContain(enCommon.next);
+        expect(response.text).toContain(enCommon.previous);
+    });
+    it("should not display pagination when there are less than 15 associations", async () => {
+        // Given
+        const mockCompanyAssociations = { ...companyAssociationsPage1 };
+        mockCompanyAssociations.totalPages = 1;
+        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(mockCompanyAssociations));
+        // When
+        const response = await router.get(`${url}?page=2`);
+        // Then
+        expect(response.text).toContain(mockCompanyAssociations.items[0].userEmail + "</th>");
+        expect(response.text).toContain(mockCompanyAssociations.items[1].userEmail + "</th>");
+        expect(response.text).not.toContain(enCommon.next);
+        expect(response.text).not.toContain(enCommon.previous);
+    });
+    it("should display the pagination links in welsh", async () => {
+        // Given
+        const mockCompanyAssociations = { ...companyAssociationsPage1 };
+        mockCompanyAssociations.totalPages = 10;
+        getCompanyAssociationsSpy.mockReturnValue(Promise.resolve(mockCompanyAssociations));
+        // When
+        const response = await router.get(`${url}?page=4&lang=cy`);
+        // Then
+        expect(response.text).toContain(mockCompanyAssociations.items[0].userEmail + "</th>");
+        expect(response.text).toContain(mockCompanyAssociations.items[1].userEmail + "</th>");
+        expect(response.text).toContain(cyCommon.next);
+        expect(response.text).toContain(cyCommon.previous);
     });
 });
