1b702843c056be4952620e33c49fdf00714d8b54
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-06-04 10:58:46 +0100

    IDVA6-1057 - adding 'personRemovedThemselves' middleware

---
+++ src/middleware/navigation/personRemovedThemselves.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import * as constants from "../../constants";
+import { redirectPage } from "../../lib/utils/referrerUtils";
+import { getExtraData } from "../../lib/utils/sessionUtils";
+
+export const removedThemselvesNavigation = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+    const referrer: string | undefined = req.get("Referrer");
+    const pageIndicator = getExtraData(req.session, constants.MANAGE_AUTHORISED_PEOPLE_INDICATOR);
+    const hrefA = getExtraData(req.session, constants.REMOVE_URL_EXTRA);
+
+    if (redirectPage(referrer, hrefA, constants.YOUR_COMPANIES_REMOVED_THEMSELVES_URL, pageIndicator)) {
+        res.redirect(constants.LANDING_URL);
+    } else {
+        next();
+    }
+};
