58a8f7a047059a8e8110012fa417b2a47d87aada
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-06-10 14:39:58 +0100

    remove apiClientService modification

--- src/services/apiClientService.ts
+++ src/services/apiClientService.ts
@@ -1,10 +1,10 @@
 import { Request } from "express";
 import { createPrivateApiClient } from "private-api-sdk-node";
-// import { INTERNAL_API_URL } from "../constants";
+import { INTERNAL_API_URL } from "../constants";
 import PrivateApiClient from "private-api-sdk-node/dist/client";
-// import { getAccessToken } from "../lib/utils/sessionUtils";
+import { getAccessToken } from "../lib/utils/sessionUtils";
 
 export function createOauthPrivateApiClient (req: Request): PrivateApiClient {
-    // const oauthAccessToken = getAccessToken(req.session);
-    return createPrivateApiClient(undefined, "wEusf3QyFlp2ckUTPxIJLloWaLGRFq7H5PO1iynU465s9NmU0lngu5sCWbWjZxeBmv0WmVs6oQg-ixVyqz1Vyl", "https://internalapi.phoenix1.aws.chdev.org", undefined);
+    const oauthAccessToken = getAccessToken(req.session);
+    return createPrivateApiClient(undefined, oauthAccessToken, INTERNAL_API_URL, undefined);
 }
