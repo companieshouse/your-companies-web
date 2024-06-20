e3d09ee0abf88fbad5b6238a0849f308fe3b1f7a
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-06-20 12:20:21 +0100

    update to  use post assocation, rather than patch

--- src/routers/controllers/createCompanyAssociationController.ts
+++ src/routers/controllers/createCompanyAssociationController.ts
@@ -1,25 +1,13 @@
 import { Request, Response } from "express";
 import * as constants from "../../constants";
-import { createAssociation, updateAssociationStatus } from "../../services/associationsService";
-import { deleteExtraData, getExtraData } from "../../lib/utils/sessionUtils";
-import { AssociationState, AssociationStateResponse } from "../../types/associations";
-import { AssociationStatus } from "private-api-sdk-node/dist/services/associations/types";
+import { createAssociation } from "../../services/associationsService";
+import { getExtraData } from "../../lib/utils/sessionUtils";
 
 export const createCompanyAssociationControllerGet = async (req: Request, res: Response): Promise<void> => {
 
     const confirmedCompanyForAssocation = getExtraData(req.session, constants.CONFIRMED_COMPANY_FOR_ASSOCIATION);
-    const associationStateResponse: AssociationStateResponse = getExtraData(req.session, constants.ASSOCIATION_STATE_RESPONSE);
 
-    if (associationStateResponse.state === AssociationState.COMPNANY_WAS_ASSOCIATED_WITH_USER ||
-        associationStateResponse.state === AssociationState.COMPNANY_AWAITING_ASSOCIATION_WITH_USER) {
-        await updateAssociationStatus(req, associationStateResponse.associationId as string, AssociationStatus.CONFIRMED);
-    } else {
-        await createAssociation(req, confirmedCompanyForAssocation.companyNumber);
-    }
+    await createAssociation(req, confirmedCompanyForAssocation.companyNumber);
 
-    deleteExtraData(req.session, constants.ASSOCIATION_STATE_RESPONSE);
-
-    const nextPageUrl = constants.YOUR_COMPANIES_COMPANY_ADDED_SUCCESS_URL;
-    return res.redirect(nextPageUrl);
-
+    return res.redirect(constants.YOUR_COMPANIES_COMPANY_ADDED_SUCCESS_URL);
 };
