30559743fe1757514f7397b898d40007b4d772cc
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2024-09-13 10:12:55 +0100

    IDVA6-1466 Data types updated and tests added

--- src/lib/utils/sessionUtils.ts
+++ src/lib/utils/sessionUtils.ts
@@ -13,7 +13,7 @@ export const getLoggedInUserEmail = (session: Session | undefined): string => {
     return signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.Email] as string;
 };
 
-export const setExtraData = (session: Session | undefined, key: string, data: any): void => {
+export const setExtraData = (session: Session | undefined, key: string, data: unknown): void => {
     return session?.setExtraData(key, data);
 };
 
@@ -21,8 +21,14 @@ export const getExtraData = (session: Session | undefined, key: string): any => 
     return session?.getExtraData(key);
 };
 
-export const deleteExtraData = (session: Session | undefined, key: string): any => {
-    return session?.deleteExtraData(key);
+/**
+ * This function will attempt to remove from extra data in a session the value that the provided key points at.
+ * @param session is the session from which the extra data should be removed
+ * @param key is the key to the value that should be removed
+ * @returns true if the function attepts to remove extra data, false if session is not provided
+ */
+export const deleteExtraData = (session: Session | undefined, key: string): boolean => {
+    return session ? session.deleteExtraData(key) : false;
 };
 
 export const getAccessToken = (session: Session | undefined): string => {
