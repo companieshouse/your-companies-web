6e59b9befe218b1b92b671a987a3ff1fcbc65599
tferns-ch <tferns@companieshouse.gov.uk>
2024-10-07 16:28:07 +0100

    IDVA6-1670: Fix remove company issues and accessibility
    - Update 'Remove company' text to 'Remove' in table view
    - Add PIWIK_CHS_DOMAIN to CSP formAction directive
    - Add title to confirmation page for accessibility

--- src/middleware/content.security.policy.middleware.config.ts
+++ src/middleware/content.security.policy.middleware.config.ts
@@ -3,6 +3,7 @@ import { HelmetOptions } from "helmet";
 export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
     const CDN = process.env.CDN_HOST as string;
     const PIWIK_URL = process.env.PIWIK_URL as string;
+    const PIWIK_CHS_DOMAIN = process.env.PIWIK_CHS_DOMAIN as string;
     const SELF = `'self'`;
     const NONCE = `'nonce-${nonce}'`;
     const ONE_YEAR_SECONDS = 31536000;
@@ -16,6 +17,10 @@ export const prepareCSPConfig = (nonce: string) : HelmetOptions => {
                 imgSrc: [CDN],
                 styleSrc: [NONCE, CDN],
                 connectSrc: [SELF, PIWIK_URL],
+                formAction: [
+                    SELF,
+                    PIWIK_CHS_DOMAIN
+                ],
                 scriptSrc: [
                     SELF,
                     NONCE,
