15adeb224ffcd8a531b752cb3e7af26077e3ac82
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-03-07 15:16:21 +0000

    IDVA6-2271 Integration tests for resent email

--- test/integration/your-companies/manage-authorised-people-email-resent/userEmail/get.test.ts
+++ test/integration/your-companies/manage-authorised-people-email-resent/userEmail/get.test.ts
@@ -1,14 +1,14 @@
-import mocks from "../../../mocks/all.middleware.mock";
-import app from "../../../../src/app";
+import mocks from "../../../../mocks/all.middleware.mock";
+import app from "../../../../../src/app";
 import supertest from "supertest";
-import * as constants from "../../../../src/constants";
-import * as associationsService from "../../../../src/services/associationsService";
+import * as constants from "../../../../../src/constants";
+import * as associationsService from "../../../../../src/services/associationsService";
 import { NextFunction, Request, Response } from "express";
 import { Session } from "@companieshouse/node-session-handler";
 
 const router = supertest(app);
 
-jest.mock("../../../../src/lib/Logger");
+jest.mock("../../../../../src/lib/Logger");
 const createAssociationSpy: jest.SpyInstance = jest.spyOn(associationsService, "createAssociation");
 const session: Session = new Session();
 mocks.mockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
@@ -20,6 +20,7 @@ describe("GET /your-companies/manage-authorised-people-email-resent/:userEmail",
 
     beforeEach(() => {
         jest.clearAllMocks();
+        session.setExtraData(constants.COMPANY_NUMBER, "1234567");
     });
 
     it("should call redirect to resent email success page when email is valid and is associated", async () => {
@@ -27,7 +28,6 @@ describe("GET /your-companies/manage-authorised-people-email-resent/:userEmail",
         const url = "/your-companies/manage-authorised-people-email-resent/bob1@bob.com";
         const expectedRedirectUrl = "/your-companies/manage-authorised-people/1234567/authorisation-email-resent";
         createAssociationSpy.mockReturnValue({ association_id: "123456" });
-        session.setExtraData(constants.COMPANY_NUMBER, "1234567");
         // When
         const response = await router.get(url);
         // Then
@@ -38,7 +38,6 @@ describe("GET /your-companies/manage-authorised-people-email-resent/:userEmail",
     it("should reject invalid emails", async () => {
         // Given
         const url = "/your-companies/manage-authorised-people-email-resent/bob1bob.com";
-        session.setExtraData(constants.COMPANY_NUMBER, "1234567");
         // When
         const response = await router.get(url);
         // Then
