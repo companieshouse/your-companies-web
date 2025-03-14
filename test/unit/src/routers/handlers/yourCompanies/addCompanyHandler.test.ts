cc87844d295ba28628a28866ae3d11a871dd2cb4
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-03-14 15:37:09 +0000

    IDVA6-2271 First unit test added

--- test/unit/src/routers/handlers/yourCompanies/addCompanyHandler.test.ts
+++ test/unit/src/routers/handlers/yourCompanies/addCompanyHandler.test.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import * as translations from "../../../../../../src/lib/utils/translations";
 import * as sessionUtils from "../../../../../../src/lib/utils/sessionUtils";
 import * as genericValidation from "../../../../../../src/lib/validation/generic";
-import * as companyProfileService from "../../../../../../src/services/companyProfileService";
+// import * as companyProfileService from "../../../../../../src/services/companyProfileService";
 import * as associationsService from "../../../../../../src/services/associationsService";
 import * as constants from "../../../../../../src/constants";
 import { mockRequest } from "../../../../../mocks/request.mock";
@@ -10,15 +10,18 @@ import { mockResponse } from "../../../../../mocks/response.mock";
 import { validActiveCompanyProfile } from "../../../../../mocks/companyProfile.mock";
 import { AssociationState } from "../../../../../../src/types/associations";
 import { AddCompanyHandler } from "../../../../../../src/routers/handlers/yourCompanies/addCompanyHandler";
+import { getCompanyProfile } from "../../../../../../src/services/companyProfileService";
 
+jest.mock("../../../../../../src/services/companyProfileService");
 jest.mock("../../../../../../src/lib/Logger");
+jest.mock("../../../../../../src/lib/utils/sessionUtils");
 const getTranslationsForViewSpy: jest.SpyInstance = jest.spyOn(translations, "getTranslationsForView");
 const getExtraDataSpy: jest.SpyInstance = jest.spyOn(sessionUtils, "getExtraData");
 const validateClearFormSpy: jest.SpyInstance = jest.spyOn(genericValidation, "validateClearForm");
-const getCompanyProfileSpy: jest.SpyInstance = jest.spyOn(companyProfileService, "getCompanyProfile");
+// const getCompanyProfileSpy: jest.SpyInstance = jest.spyOn(companyProfileService, "getCompanyProfile");
 const isOrWasCompanyAssociatedWithUserSpy: jest.SpyInstance = jest.spyOn(associationsService, "isOrWasCompanyAssociatedWithUser");
 const req: Request = mockRequest();
-const resp: Response = mockResponse();
+const res: Response = mockResponse();
 
 describe("AddCompanyHandler", () => {
     const addCompanyHandler: AddCompanyHandler = new AddCompanyHandler();
@@ -30,22 +33,35 @@ describe("AddCompanyHandler", () => {
     test.each([
         {
             referer: "",
-            isClearForm: false,
+            isClearForm: true,
             method: constants.POST,
             body: { companyNumber: validActiveCompanyProfile.companyNumber },
+            query: { [constants.CLEAR_FORM]: "true" },
             companyProfile: validActiveCompanyProfile,
-            isAssociated: { state: AssociationState.COMPANY_AWAITING_ASSOCIATION_WITH_USER }
+            isAssociated: {
+                state: AssociationState.COMPANY_AWAITING_ASSOCIATION_WITH_USER,
+                associationId: "12345678"
+            },
+            viewData: {
+                templateName: constants.ADD_COMPANY_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: { test: "Test" },
+                proposedCompanyNumber: validActiveCompanyProfile.companyNumber
+            }
         }
-    ])("",
-        async ({ referer, isClearForm, body, companyProfile, method, isAssociated }) => {
+    ])("should return expected viewData object if method is $method",
+        async ({ referer, isClearForm, body, query, companyProfile, method, isAssociated, viewData }) => {
             // Given
             req.headers.referer = referer;
             req.body = body;
-            getTranslationsForViewSpy.mockReturnValue({});
+            req.query = query;
+            getTranslationsForViewSpy.mockReturnValue({ test: "Test" });
             validateClearFormSpy.mockReturnValue(isClearForm);
-            getCompanyProfileSpy.mockReturnValue(companyProfile);
+            (getCompanyProfile as jest.Mock).mockReturnValue(companyProfile);
+            isOrWasCompanyAssociatedWithUserSpy.mockReturnValue(isAssociated);
             // When
-            const response = await addCompanyHandler.execute(req, resp, method);
+            const response = await addCompanyHandler.execute(req, res, method);
             // Then
+            expect(response).toEqual(viewData);
         });
 });
