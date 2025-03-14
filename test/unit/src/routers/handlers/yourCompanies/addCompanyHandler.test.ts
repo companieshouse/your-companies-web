4eee4b06b90edab60c7a44c13be6488178ab79bf
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-03-14 17:25:00 +0000

    IDVA6-2271 Unit tests added

--- test/unit/src/routers/handlers/yourCompanies/addCompanyHandler.test.ts
+++ test/unit/src/routers/handlers/yourCompanies/addCompanyHandler.test.ts
@@ -7,16 +7,16 @@ import * as associationsService from "../../../../../../src/services/association
 import * as constants from "../../../../../../src/constants";
 import { mockRequest } from "../../../../../mocks/request.mock";
 import { mockResponse } from "../../../../../mocks/response.mock";
-import { validActiveCompanyProfile } from "../../../../../mocks/companyProfile.mock";
+import { validActiveCompanyProfile, validDisolvedCompanyProfile } from "../../../../../mocks/companyProfile.mock";
 import { AssociationState } from "../../../../../../src/types/associations";
 import { AddCompanyHandler } from "../../../../../../src/routers/handlers/yourCompanies/addCompanyHandler";
 import { getCompanyProfile } from "../../../../../../src/services/companyProfileService";
+import { when } from "jest-when";
 
 jest.mock("../../../../../../src/services/companyProfileService");
 jest.mock("../../../../../../src/lib/Logger");
 jest.mock("../../../../../../src/lib/utils/sessionUtils");
 const getTranslationsForViewSpy: jest.SpyInstance = jest.spyOn(translations, "getTranslationsForView");
-const getExtraDataSpy: jest.SpyInstance = jest.spyOn(sessionUtils, "getExtraData");
 const validateClearFormSpy: jest.SpyInstance = jest.spyOn(genericValidation, "validateClearForm");
 // const getCompanyProfileSpy: jest.SpyInstance = jest.spyOn(companyProfileService, "getCompanyProfile");
 const isOrWasCompanyAssociatedWithUserSpy: jest.SpyInstance = jest.spyOn(associationsService, "isOrWasCompanyAssociatedWithUser");
@@ -32,7 +32,8 @@ describe("AddCompanyHandler", () => {
 
     test.each([
         {
-            referer: "",
+            condition: "company is awaiting association with user",
+            referer: "/your-companies",
             isClearForm: true,
             method: constants.POST,
             body: { companyNumber: validActiveCompanyProfile.companyNumber },
@@ -48,8 +49,86 @@ describe("AddCompanyHandler", () => {
                 lang: { test: "Test" },
                 proposedCompanyNumber: validActiveCompanyProfile.companyNumber
             }
+        },
+        {
+            condition: "company is not associated with user",
+            referer: "/your-companies",
+            isClearForm: true,
+            method: constants.POST,
+            body: { companyNumber: validActiveCompanyProfile.companyNumber },
+            query: { [constants.CLEAR_FORM]: "true" },
+            companyProfile: validActiveCompanyProfile,
+            isAssociated: {
+                state: AssociationState.COMPANY_NOT_ASSOCIATED_WITH_USER,
+                associationId: "12345678"
+            },
+            viewData: {
+                templateName: constants.ADD_COMPANY_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: { test: "Test" },
+                proposedCompanyNumber: validActiveCompanyProfile.companyNumber
+            }
+        },
+        {
+            condition: "company was associated with user",
+            referer: "/your-companies",
+            isClearForm: true,
+            method: constants.POST,
+            body: { companyNumber: validActiveCompanyProfile.companyNumber },
+            query: { [constants.CLEAR_FORM]: "true" },
+            companyProfile: validActiveCompanyProfile,
+            isAssociated: {
+                state: AssociationState.COMPANY_WAS_ASSOCIATED_WITH_USER,
+                associationId: "12345678"
+            },
+            viewData: {
+                templateName: constants.ADD_COMPANY_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: { test: "Test" },
+                proposedCompanyNumber: validActiveCompanyProfile.companyNumber
+            }
+        },
+        {
+            condition: "company is associated with user",
+            referer: "/your-companies",
+            isClearForm: true,
+            method: constants.POST,
+            body: { companyNumber: validActiveCompanyProfile.companyNumber },
+            query: { [constants.CLEAR_FORM]: "true" },
+            companyProfile: validActiveCompanyProfile,
+            isAssociated: {
+                state: AssociationState.COMPANY_ASSOCIATED_WITH_USER,
+                associationId: "12345678"
+            },
+            viewData: {
+                templateName: constants.ADD_COMPANY_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: { test: "Test" },
+                proposedCompanyNumber: validActiveCompanyProfile.companyNumber,
+                errors: { companyNumber: { text: constants.THIS_COMPANY_HAS_ALREADY_BEEN_ADDED_TO_YOUR_ACCOUNT } }
+            }
+        },
+        {
+            condition: "company is disolved",
+            referer: "/your-companies",
+            isClearForm: true,
+            method: constants.POST,
+            body: { companyNumber: validDisolvedCompanyProfile.companyNumber },
+            query: { [constants.CLEAR_FORM]: "true" },
+            companyProfile: validDisolvedCompanyProfile,
+            isAssociated: {
+                state: AssociationState.COMPANY_AWAITING_ASSOCIATION_WITH_USER,
+                associationId: "12345678"
+            },
+            viewData: {
+                templateName: constants.ADD_COMPANY_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: { test: "Test" },
+                proposedCompanyNumber: validDisolvedCompanyProfile.companyNumber,
+                errors: { companyNumber: { text: constants.ENTER_A_COMPANY_NUMBER_FOR_A_COMPANY_THAT_IS_ACTIVE } }
+            }
         }
-    ])("should return expected viewData object if method is $method",
+    ])("should return expected viewData object if method is $method and $condition",
         async ({ referer, isClearForm, body, query, companyProfile, method, isAssociated, viewData }) => {
             // Given
             req.headers.referer = referer;
@@ -58,6 +137,9 @@ describe("AddCompanyHandler", () => {
             getTranslationsForViewSpy.mockReturnValue({ test: "Test" });
             validateClearFormSpy.mockReturnValue(isClearForm);
             (getCompanyProfile as jest.Mock).mockReturnValue(companyProfile);
+            when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.PROPOSED_COMPANY_NUM).mockReturnValue(body.companyNumber);
+            when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.COMPANY_PROFILE).mockReturnValue(companyProfile);
+            // when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.CURRENT_COMPANY_NUM).mockReturnValue(proposedCompanyNumber);
             isOrWasCompanyAssociatedWithUserSpy.mockReturnValue(isAssociated);
             // When
             const response = await addCompanyHandler.execute(req, res, method);
