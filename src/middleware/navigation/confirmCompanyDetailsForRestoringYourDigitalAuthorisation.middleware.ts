a8e2bfa934fbdd9907d558c94f8b7d8066ad432f
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-06-10 16:04:35 +0100

    SIV-283 Navigation middleware refactored

--- src/middleware/navigation/confirmCompanyDetailsForRestoringYourDigitalAuthorisation.middleware.ts
+++ src/middleware/navigation/confirmCompanyDetailsForRestoringYourDigitalAuthorisation.middleware.ts
@@ -1,74 +1,42 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import * as constants from "../../constants";
 import { redirectPage } from "../../lib/utils/referrerUtils";
 import logger, { createLogMessage } from "../../lib/Logger";
-import { deleteExtraData } from "../../lib/utils/sessionUtils";
+import {
+    getConfirmCompanyDetailsForRestoringYourDigitalAuthorisationFullUrl
+} from "../../lib/utils/urlUtils";
 
 /**
  * Middleware to handle navigation logic for confirming a company
  * details for restoring your digital authorisation.
- * It clears specific session data, logs the request, and redirects
- * to the landing page if certain conditions are met. Otherwise, it
- * proceeds to the next middleware.
+ * Redirects the user to the landing page if certain conditions are met,
+ * otherwise passes control to the next middleware.
  *
- * @param req - The HTTP request object.
- * @param res - The HTTP response object.
- * @param next - The next middleware function in the stack.
+ * @param req - The Express request object
+ * @param res - The Express response object
+ * @param next - The next middleware function
  */
-export const confirmCompanyDetailsForRestoringYourDigitalAuthorisationNavigation = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
+export const confirmCompanyDetailsForRestoringYourDigitalAuthorisationNavigation = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const referrer = req.get("Referrer");
-    const pageIndicator = false;
+    const companyNumber: string = req.params[constants.COMPANY_NUMBER];
 
-    clearSessionData(req);
-    logRequest(req);
-
-    if (shouldRedirect(referrer, pageIndicator)) {
-        res.redirect(constants.LANDING_URL);
-    } else {
-        next();
-    }
-};
-
-/**
- * Clears specific session data related to company navigation.
- *
- * @param req - The HTTP request object.
- */
-const clearSessionData = (req: Request): void => {
-    deleteExtraData(req.session, constants.MANAGE_AUTHORISED_PEOPLE_INDICATOR);
-    deleteExtraData(req.session, constants.CURRENT_COMPANY_NUM);
-};
-
-/**
- * Logs the navigation request for debugging purposes.
- *
- * @param req - The HTTP request object.
- */
-const logRequest = (req: Request): void => {
     logger.debug(
         createLogMessage(
             req.session,
             confirmCompanyDetailsForRestoringYourDigitalAuthorisationNavigation.name,
             `request to ${req.originalUrl}, calling redirectPage fn`
         ));
-};
 
-/**
- * Determines whether the user should be redirected based on the referrer and page indicator.
- *
- * @param referrer - The referrer URL from the request headers.
- * @param pageIndicator - A boolean indicating the page state.
- * @returns A boolean indicating whether a redirect should occur.
- */
-const shouldRedirect = (referrer: string | undefined, pageIndicator: boolean): boolean => {
-    return redirectPage(
+    const shouldRedirect = redirectPage(
         referrer,
         constants.LANDING_URL,
-        constants.CONFIRM_COMPANY_DETAILS_FOR_RESTORING_YOUR_DIGITAL_AUTHORISATION_URL,
-        pageIndicator
+        getConfirmCompanyDetailsForRestoringYourDigitalAuthorisationFullUrl(companyNumber),
+        false
     );
+
+    if (shouldRedirect) {
+        res.redirect(constants.LANDING_URL);
+    } else {
+        next();
+    }
 };
