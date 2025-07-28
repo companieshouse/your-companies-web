e4f844bb78e28a4060f046062a18e53fc83bbe3c
bwallace-ch <bwallace@companieshouse.gov.uk>
2025-07-28 12:17:01 +0100

    SIV-504 - updating incomplete unit test

--- test/unit/src/routers/handlers/yourCompanies/confirmationAuthorisationRemovedHandler.test.ts
+++ test/unit/src/routers/handlers/yourCompanies/confirmationAuthorisationRemovedHandler.test.ts
@@ -23,49 +23,41 @@ describe("ConfirmationAuthorisationRemovedHandler", () => {
         confirmationAuthorisationRemovedHandler = new ConfirmationAuthorisationRemovedHandler();
     });
 
-    test.each([
-        {
-            condition: "Basic test that doesn't do anything NEEDS TO CHANGE",
-            method: constants.GET,
-            viewData: {
-                yourCompaniesHref: constants.LANDING_URL
+    it("should return expected view data", async () => {
+        // Given
+        const lang = "en";
+
+        const companyNumber = validActiveCompanyProfile.companyNumber;
+        const companyName = validActiveCompanyProfile.companyName;
+        when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.REMOVE_AUTHORISATION_COMPANY_NAME).mockReturnValue(companyName);
+        when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.REMOVE_AUTHORISATION_COMPANY_NUMBER).mockReturnValue(companyNumber);
+
+        const req: Request = mockParametrisedRequest({
+            session: new Session(),
+            lang,
+            params: {
+                companyNumber
             }
-        }
-    ])("should return expected viewData object if method is $method and $condition",
-        async ({ viewData }) => {
-            // Given
-            const lang = "en";
-
-            const companyNumber = validActiveCompanyProfile.companyNumber;
-            const companyName = validActiveCompanyProfile.companyName;
-            when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.REMOVE_AUTHORISATION_COMPANY_NAME).mockReturnValue(companyName);
-            when(sessionUtils.getExtraData).calledWith(expect.anything(), constants.REMOVE_AUTHORISATION_COMPANY_NUMBER).mockReturnValue(companyNumber);
-
-            const req: Request = mockParametrisedRequest({
-                session: new Session(),
-                lang,
-                params: {
-                    companyNumber
-                }
-            });
-            const translations = { key: "value" };
-            getTranslationsForViewSpy.mockReturnValueOnce(translations);
-
-            const expectedViewData = {
-                templateName: constants.CONFIRMATION_AUTHORISATION_REMOVED_PAGE,
-                lang: translations,
-                companyName,
-                companyNumber,
-                ...viewData
-            };
-            // When
-            const response = await confirmationAuthorisationRemovedHandler.execute(req);
-            // Then
-            expect(getTranslationsForViewSpy).toHaveBeenCalledTimes(1);
-            expect(getTranslationsForViewSpy).toHaveBeenCalledWith(lang, constants.CONFIRMATION_AUTHORISATION_REMOVED_PAGE);
-            expect(getExtraDataSpy).toHaveBeenCalledTimes(2);
-            expect(getExtraDataSpy).toHaveBeenCalledWith(expect.any(Session), constants.REMOVE_AUTHORISATION_COMPANY_NAME);
-            expect(getExtraDataSpy).toHaveBeenCalledWith(expect.any(Session), constants.REMOVE_AUTHORISATION_COMPANY_NUMBER);
-            expect(response).toEqual(expectedViewData);
         });
+        const translations = { key: "value" };
+        getTranslationsForViewSpy.mockReturnValueOnce(translations);
+
+        const expectedViewData = {
+            templateName: constants.CONFIRMATION_AUTHORISATION_REMOVED_PAGE,
+            lang: translations,
+            companyName,
+            companyNumber,
+            yourCompaniesHref: constants.LANDING_URL
+        };
+
+        // When
+        const response = await confirmationAuthorisationRemovedHandler.execute(req);
+        // Then
+        expect(getTranslationsForViewSpy).toHaveBeenCalledTimes(1);
+        expect(getTranslationsForViewSpy).toHaveBeenCalledWith(lang, constants.CONFIRMATION_AUTHORISATION_REMOVED_PAGE);
+        expect(getExtraDataSpy).toHaveBeenCalledTimes(2);
+        expect(getExtraDataSpy).toHaveBeenCalledWith(expect.any(Session), constants.REMOVE_AUTHORISATION_COMPANY_NAME);
+        expect(getExtraDataSpy).toHaveBeenCalledWith(expect.any(Session), constants.REMOVE_AUTHORISATION_COMPANY_NUMBER);
+        expect(response).toEqual(expectedViewData);
+    });
 });
