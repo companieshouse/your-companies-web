9ee57648ec4cf9573fa7bc568ef62071afc7043c
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-07-28 15:59:27 +0100

    SIV-481 Unit and integration tests added to person digital authorisation cancelled journey

---
+++ test/unit/src/routers/handlers/yourCompanies/confirmationPersonsDigitalAuthorisationCancelledHandler.test.ts
@@ -0,0 +1,66 @@
+import { ConfirmationPersonsDigitalAuthorisationCancelledHandler } from "../../../../../../src/routers/handlers/yourCompanies/confirmationPersonsDigitalAuthorisationCancelledHandler";
+import { mockParametrisedRequest } from "../../../../../mocks/request.mock";
+import { Request } from "express";
+import * as constants from "../../../../../../src/constants";
+import * as translations from "../../../../../../src/lib/utils/translations";
+import { Session } from "@companieshouse/node-session-handler";
+import * as sessionUtils from "../../../../../../src/lib/utils/sessionUtils";
+import * as urlUtils from "../../../../../../src/lib/utils/urlUtils";
+import { PersonRemovedConfirmation } from "../../../../../../src/types/removal";
+
+jest.mock("../../../../../../src/lib/Logger");
+
+const getTranslationsForViewSpy: jest.SpyInstance = jest.spyOn(translations, "getTranslationsForView");
+const getExtraDataSpy: jest.SpyInstance = jest.spyOn(sessionUtils, "getExtraData");
+const getManageAuthorisedPeopleFullUrlSpy: jest.SpyInstance = jest.spyOn(urlUtils, "getManageAuthorisedPeopleFullUrl");
+
+describe("ConfirmationPersonsDigitalAuthorisationCancelledHandler", () => {
+    let confirmationPersonsDigitalAuthorisationCancelledHandler: ConfirmationPersonsDigitalAuthorisationCancelledHandler;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.resetAllMocks();
+        confirmationPersonsDigitalAuthorisationCancelledHandler = new ConfirmationPersonsDigitalAuthorisationCancelledHandler();
+    });
+
+    test.each([
+        { lang: "en" }, { lang: "cy" }
+    ])("should return expected view data when language set to '$lang'",
+        async ({ lang }) => {
+            // Given
+            const req: Request = mockParametrisedRequest({ session: new Session(), lang });
+            const translations = { key: "value" };
+            getTranslationsForViewSpy.mockReturnValue(translations);
+            const companyName = "Test Ltd.";
+            const companyNumber = "12345678";
+            const userNameOrEmail = "test@example.com";
+            const personRemovedConfirmationData: PersonRemovedConfirmation = {
+                userNameOrEmail,
+                companyNumber,
+                companyName
+            };
+            getExtraDataSpy.mockReturnValueOnce(personRemovedConfirmationData);
+            const managePeopleHref = `/your-companies/manage-authorised-people/${companyNumber}`;
+            getManageAuthorisedPeopleFullUrlSpy.mockReturnValue(managePeopleHref);
+            const viewData = {
+                templateName: constants.CONFIRMATION_PERSONS_DIGITAL_AUTHORISATION_CANCELLED_PAGE,
+                backLinkHref: constants.LANDING_URL,
+                lang: translations,
+                companyName,
+                companyNumber,
+                userEmail: userNameOrEmail,
+                managePeopleHref
+            };
+            // When
+            const response = await confirmationPersonsDigitalAuthorisationCancelledHandler.execute(req);
+            // Then
+            expect(getExtraDataSpy).toHaveBeenCalledTimes(1);
+            expect(getExtraDataSpy).toHaveBeenCalledWith(expect.any(Session), constants.PERSON_REMOVED_CONFIRMATION_DATA);
+            expect(getTranslationsForViewSpy).toHaveBeenCalledTimes(1);
+            expect(getTranslationsForViewSpy).toHaveBeenCalledWith(lang, constants.CONFIRMATION_PERSONS_DIGITAL_AUTHORISATION_CANCELLED_PAGE);
+            expect(getManageAuthorisedPeopleFullUrlSpy).toHaveBeenCalledTimes(1);
+            expect(getManageAuthorisedPeopleFullUrlSpy).toHaveBeenCalledWith(constants.MANAGE_AUTHORISED_PEOPLE_URL, companyNumber);
+            expect(response).toEqual(viewData);
+
+        });
+});
