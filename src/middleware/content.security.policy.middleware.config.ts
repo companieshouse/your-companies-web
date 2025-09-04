e0671e88344af75b623603eb4fd2635d82e1afc0
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2025-09-04 12:15:10 +0100

    SIV-707 content security policy - allow http and https CHS-URL form submissions

--- src/middleware/content.security.policy.middleware.config.ts
+++ src/middleware/content.security.policy.middleware.config.ts
@@ -10,7 +10,8 @@ export const prepareCSPConfig = (nonce: string): HelmetOptions => {
     const CDN = process.env.CDN_HOST as string;
     const PIWIK_URL = process.env.PIWIK_URL as string;
     const PIWIK_CHS_DOMAIN = process.env.PIWIK_CHS_DOMAIN as string;
-    const CHS_URL = process.env.CHS_URL;
+    const CHS_URL = process.env.CHS_URL as string;
+    const HTTP_CHS_URL: string = CHS_URL.replace(/^https:\/\//, "http://") as string;
     const SELF = `'self'`;
     const NONCE = `'nonce-${nonce}'`;
     const ONE_YEAR_SECONDS = 31536000;
@@ -28,8 +29,8 @@ export const prepareCSPConfig = (nonce: string): HelmetOptions => {
                     SELF,
                     `https://${PIWIK_CHS_DOMAIN}`,
                     `http://${PIWIK_CHS_DOMAIN}`,
-                    `https://${CHS_URL}`,
-                    `http://${CHS_URL}`
+                    CHS_URL,
+                    HTTP_CHS_URL
                 ],
                 scriptSrc: [SELF, NONCE, CDN, PIWIK_URL],
                 objectSrc: [`'none'`]
