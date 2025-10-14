8d92ed534b86a85daa16e09e2865001d43ae5d30
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-10-14 13:41:35 +0100

    SIV-904 Referer check added to verify if redirection to auth service happened

--- src/routers/handlers/yourCompanies/companyAddSuccessHandler.ts
+++ src/routers/handlers/yourCompanies/companyAddSuccessHandler.ts
@@ -32,17 +32,28 @@ export class CompanyAddSuccessHandler extends GenericHandler {
     }
 
     /**
-     * Executes the handler logic to prepare the view data for the company add success page.
+     * Prepares and returns the view data for the company add success page.
      *
-     * @param req - The HTTP request object.
-     * @returns A promise that resolves to the view data for the page.
+     * Retrieves the company information from the session, checks for invalid navigation,
+     * populates the view data with the company name, number, translations, and button link.
+     *
+     * @param req - The HTTP request object containing session and language information.
+     * @returns A promise that resolves to the populated view data for the company add success page.
+     * @throws Error if the user navigated from the confirm company details page and the authentication code is already present in the session.
      */
     async execute (req: Request): Promise<CompanyAddSuccessViewData> {
         const companyNowAssociated = getExtraData(req.session, constants.CONFIRMED_COMPANY_FOR_ASSOCIATION);
+
+        if (req.headers.referer?.includes(constants.CONFIRM_COMPANY_DETAILS_URL)) {
+            throw new Error(
+                `Failed to add company with companyNumber '${companyNowAssociated.companyNumber}': authentication code already present in session.`
+            );
+        }
+
         this.viewData.companyName = companyNowAssociated.companyName.toUpperCase();
         this.viewData.companyNumber = companyNowAssociated.companyNumber;
         this.viewData.lang = getTranslationsForView(req.lang, constants.COMPANY_ADD_SUCCESS_PAGE);
         this.viewData.buttonHref = constants.LANDING_URL;
-        return Promise.resolve(this.viewData);
+        return this.viewData;
     }
 }
