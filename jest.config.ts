f378fea08884e5b0c36018280a357319d8d2505e
John Griffiths <jgriffiths2@companieshouse.gov.uk>
2026-04-30 14:27:49 +0100

    IDVA6-1999 bump vulnerable dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,14 @@
 module.exports = {
-    preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     moduleFileExtensions: ['ts', 'js', 'json'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
+        '^.+\\.js$': 'babel-jest',
     },
+    transformIgnorePatterns: [
+        '/node_modules/(?!(uuid)/)',
+    ],
     moduleDirectories: ['node_modules', 'src'],
     globalSetup: "./test/global.setup.ts",
     testTimeout: 10000
