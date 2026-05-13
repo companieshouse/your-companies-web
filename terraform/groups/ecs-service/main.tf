5f334ad9b4aacfd095ac790dc21edceb422c9bbf
carolineward2 <cward1@companieshouse.gov.uk>
2026-05-13 14:54:17 +0100

    update terraform secrets

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.296"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.326"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
